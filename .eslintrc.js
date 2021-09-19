0c494b793361efa2a76bac8e76916aaa068be534
tinopai <eltinopa@icloud.com>
2021-09-19 17:10:49 -0300

    feat: Added user available and more shit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,24 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    env: {
-        node: true,
-        es2021: true,
-    },
-    extends: [
-        'eslint:recommended', 'google',
-    ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 12,
-        sourceType: 'module',
-    },
-    plugins: [
-        '@typescript-eslint',
-    ],
-    rules: {
-        'indent': ['error', 4],
-        'max-len': ['warn', 120],
-        'new-cap': ['off'],
-    },
+	env: {
+		node: true,
+		es2021: true,
+	},
+	extends: [
+		'eslint:recommended', 'google',
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 12,
+		sourceType: 'module',
+	},
+	plugins: [
+		'@typescript-eslint',
+	],
+	rules: {
+		'indent': ['error', 'tab'],
+		'max-len': ['warn', 120],
+		'new-cap': ['off'],
+        'no-tabs': false,
+	},
 };
--- main.ts
+++ main.ts
@@ -7,50 +7,62 @@ import chalk from 'chalk';
 import dotenv from 'dotenv';
 dotenv.config();
 
-// API default handler
-app.get('/', (req, res) => {
-    res.status(200).json({
-        success: true,
-        message: 'Welcome to the API',
-    });
+// CORS
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Headers', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	next();
 });
 
+// API default handler
+app.get('/', (req, res) => {
+	res.status(200).json({
+		success: true,
+		message: 'Welcome to the API',
+	});
+});
+
+// Import database
 import db from './utility/database';
-
-// Username available check
-app.get('/isAvailable', express.json(), async (req, res) => {
-    if (!req?.body?.username) {
-        res.status(400).json({
-            success: false,
-            message: 'Invalid request',
-        });
-    }
-
-    const foundUser = await db.get('users').findOne({
-        username: req.body.username,
-    });
-
-    return res.status(200).json({
-        success: true,
-        message: `Username is ${foundUser ? 'not ' : ''}available`,
-        available: foundUser == undefined,
-    });
+// Username & email available check
+app.post('/isAvailable', express.json(), async (req, res) => {
+	if (!req?.body?.username || !req?.body?.email) {
+		res.status(400).json({
+			success: false,
+			message: 'Invalid request',
+		}).end();
+	}
+	
+	const foundUser = await db.get('users').findOne({
+		$or: [
+			{username: req.body.username},
+			{email: req.body.email},
+		],
+	});
+	
+	return res.status(200).json({
+		success: true,
+		message: `Username/Email is ${foundUser ? 'not ' : ''}available`,
+		available: foundUser == undefined,
+	}).end();
 });
 
 // Confirm registration on phone
 app.get('/register', (req, res) => {
-    console.log(req.query);
-    res.status(200).json({
-        success: true,
-        query: req.query,
-    });
+	console.log(req.query);
+	res.status(200).json({
+		success: true,
+		query: req.query,
+	});
 });
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    console.info(
-        chalk.bgGreenBright(
-            chalk.black(`[qr2fa] Listening to ${PORT}`),
-        ),
-    );
-});
+	console.info(
+		chalk.bgGreenBright(
+			chalk.black(`[qr2fa] Listening to ${PORT}`),
+			),
+			);
+		});
+		
